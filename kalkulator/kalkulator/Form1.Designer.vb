2e3928a3335b65c0a6acdfafd28449f5f6f7bc35
Ajiksuryowidodo <ajikpredator1922@gmail.com>
2015-10-02 17:32:46 +0700

    membuat perbandingan

---
+++ kalkulator/kalkulator/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.hitungbtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.HASILLABEL = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'hitungbtn
+        '
+        Me.hitungbtn.Location = New System.Drawing.Point(172, 114)
+        Me.hitungbtn.Name = "hitungbtn"
+        Me.hitungbtn.Size = New System.Drawing.Size(97, 26)
+        Me.hitungbtn.TabIndex = 0
+        Me.hitungbtn.Text = "HITUNG"
+        Me.hitungbtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Elephant", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(93, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(176, 32)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Elephant", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(93, 65)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(176, 32)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "angka 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "angka 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "HASIL"
+        '
+        'HASILLABEL
+        '
+        Me.HASILLABEL.AutoSize = True
+        Me.HASILLABEL.Location = New System.Drawing.Point(90, 114)
+        Me.HASILLABEL.Name = "HASILLABEL"
+        Me.HASILLABEL.Size = New System.Drawing.Size(13, 13)
+        Me.HASILLABEL.TabIndex = 6
+        Me.HASILLABEL.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 153)
+        Me.Controls.Add(Me.HASILLABEL)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.hitungbtn)
+        Me.Name = "Form1"
+        Me.Text = "kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents hitungbtn As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents HASILLABEL As System.Windows.Forms.Label
+
+End Class
---
+++ kalkulator/kalkulator/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub hitungbtn_Click(sender As Object, e As EventArgs) Handles hitungbtn.Click
+        Dim angka1, angka2, hasil As Integer
+        'Dim angka1 As Interger
+        'Dim angka2 As Interger
+        'Dim hasil As Interger
+        angka1 = Convert.ToInt32(TextBox1.Text)
+        angka2 = Convert.ToInt32(TextBox2.Text)
+        hasil = angka1 + angka2
+        HASILLABEL.Text = hasil.ToString()
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByValsender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        inputfilter(e)
+    End Sub
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        inputfilter(e)
+    End Sub
+    Private Sub inputfilter(ByVal e As KeyPressEventArgs)
+        MessageBox.Show(Asc(e.KeyChar))
+        If (Asc(e.KeyChar) < 48 Or _
+            Asc(e.KeyChar) > 57) And_
+            Asc(e.KeyChar) <> 8 Then
+            e.Handled = True
+        Else
+            e.Handled = True
+        End If
+    End Sub
+End Class
